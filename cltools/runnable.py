2fbbda5c0645e4c4dba2540f5ac51bfe37a4b90d
gissehel <public-devgit-cochir@gissehel.org>
2013-05-04 17:10:26 +0200

    Global options can now be specified BEFORE the command name

--- cltools/runnable.py
+++ cltools/runnable.py
@@ -29,7 +29,7 @@ class CLRunnable(object) :
 
     def __print_general_parameters(self) :
         if len(self._cl_params['params'])>0 :
-            print 'General parameters:'
+            print 'Global options:'
             names = sorted(set(self._cl_params['params'][param_name]['name'] for param_name in self._cl_params['params']))
             for name in names :
                 param = self._cl_params['params'][name]
@@ -48,7 +48,7 @@ class CLRunnable(object) :
         else :
             tool_name = tool_name + ' '
 
-        print _("Usage: %sCOMMAND_NAME [OPTIONS] [VALUES]") % (tool_name,)
+        print _("Usage: %s[GLOBAL OPTIONS] COMMAND_NAME [OPTIONS] [VALUES]") % (tool_name,)
         if self._cl_params['doc'] is not None :
             print self._cl_params['doc']
         print ''
@@ -70,7 +70,7 @@ class CLRunnable(object) :
             print command['doc']
         print ''
         if len(command['params'])>0 :
-            print 'Command parameters:'
+            print 'Command options:'
             names = sorted(set(command['params'][param_name]['name'] for param_name in command['params']))
             for name in names :
                 param = command['params'][name]
@@ -93,75 +93,78 @@ class CLRunnable(object) :
         else :
             self._tool_name = os.path.basename(self._tool_name)
 
-        if len(args) == 1 :
-            self.help()
-            self.errorexit(_("Need a command name"))
-        else :
-            command_name = args[1]
-            needed_arguments = []
-            if command_name in self._cl_params['commands'] :
-                ordered_args = []
-                dict_args = {}
-                parameter_hooks = []
-                command = self._cl_params['commands'][command_name]
+        command_name = None
+        needed_arguments = []
+        ordered_args = []
+        command = None
+        dict_args = {}
+        parameter_hooks = []
 
-                for arg in args[2:] :
-                    if arg.startswith('--') :
-                        if len(needed_arguments) > 0 :
-                            (prev_arg_letter,prev_arg,prev_param) = needed_arguments[0]
-                            self.errorexit(_("Switch [-%s] need parameter in [%s]") % (prev_arg_letter,prev_arg))
-                        arg_parts = arg.split('=',1)
-                        arg_name = arg_parts[0][2:]
-                        if arg_name not in command['params'] :
-                            if arg_name not in self._cl_params['params'] :
-                                self.errorexit(_("Don't know [%s] in param [%s]" % (arg_name, arg)))
-                            else :
-                                param = self._cl_params['params'][arg_name]
-                        else :
-                            param = command['params'][arg_name]
-
-                        dict_args[param['name']] = arg_parts[1] if len(arg_parts) > 1 else param['default']
-                        if param['code'] is not None :
-                            parameter_hooks.append((param['code'],param['name'],dict_args[param['name']]))
-
-                    elif arg.startswith('-') and len(arg)>1 :
-                        if len(needed_arguments) > 0 :
-                            (prev_arg_letter,prev_arg,prev_param) = needed_arguments[0]
-                            self.errorexit(_("Switch [-%s] need parameter in [%s]") % (prev_arg_letter,prev_arg))
-                        for arg_letter in arg[1:] :
-                            if arg_letter not in command['params'] :
-                                if arg_letter not in self._cl_params['params'] :
-                                    self.errorexit(_("Don't know [%s] in switch [%s]") % (arg_letter, arg))
-                                else :
-                                    param = self._cl_params['params'][arg_letter]
-                            else :
-                                param = command['params'][arg_letter]
-
-                            if param['need_value'] :
-                                needed_arguments.append((arg_letter,arg,param))
-                            else :
-                                dict_args[param['name']] = param['default']
-                                if param['code'] is not None :
-                                    parameter_hooks.append((param['code'],param['name'],dict_args[param['name']]))
-                    else :
-                        if len(needed_arguments) > 0 :
-                            (prev_arg_letter,prev_arg,prev_param) = needed_arguments[0]
-                            dict_args[prev_param['name']] = arg
-                            if prev_param['code'] is not None :
-                                parameter_hooks.append((prev_param['code'],prev_param['name'],dict_args[prev_param['name']]))
-
-                            needed_arguments.pop(0)
-                        else :
-                            ordered_args.append(arg)
+        for arg in args[1:] :
+            if arg.startswith('--') :
                 if len(needed_arguments) > 0 :
                     (prev_arg_letter,prev_arg,prev_param) = needed_arguments[0]
                     self.errorexit(_("Switch [-%s] need parameter in [%s]") % (prev_arg_letter,prev_arg))
-                for code, name, value in parameter_hooks :
-                    code(self, args=ordered_args, kwargs=dict_args, name=name, value=value, command=command)
-                command['code'](self,args=ordered_args,kwargs=dict_args)
+                arg_parts = arg.split('=',1)
+                arg_name = arg_parts[0][2:]
+                if command is None or arg_name not in command['params'] :
+                    if arg_name not in self._cl_params['params'] :
+                        self.errorexit(_("Don't know [%s] in param [%s]" % (arg_name, arg)))
+                    else :
+                        param = self._cl_params['params'][arg_name]
+                else :
+                    param = command['params'][arg_name]
+
+                dict_args[param['name']] = arg_parts[1] if len(arg_parts) > 1 else param['default']
+                if param['code'] is not None :
+                    parameter_hooks.append((param['code'],param['name'],dict_args[param['name']]))
+
+            elif arg.startswith('-') and len(arg)>1 :
+                if len(needed_arguments) > 0 :
+                    (prev_arg_letter,prev_arg,prev_param) = needed_arguments[0]
+                    self.errorexit(_("Switch [-%s] need parameter in [%s]") % (prev_arg_letter,prev_arg))
+                for arg_letter in arg[1:] :
+                    if command is None or arg_letter not in command['params'] :
+                        if arg_letter not in self._cl_params['params'] :
+                            self.errorexit(_("Don't know [%s] in switch [%s]") % (arg_letter, arg))
+                        else :
+                            param = self._cl_params['params'][arg_letter]
+                    else :
+                        param = command['params'][arg_letter]
+
+                    if param['need_value'] :
+                        needed_arguments.append((arg_letter,arg,param))
+                    else :
+                        dict_args[param['name']] = param['default']
+                        if param['code'] is not None :
+                            parameter_hooks.append((param['code'],param['name'],dict_args[param['name']]))
             else :
-                self.help()
-                self.errorexit(_("No command named [%s]") % (command_name,))
+                if len(needed_arguments) > 0 :
+                    (prev_arg_letter,prev_arg,prev_param) = needed_arguments[0]
+                    dict_args[prev_param['name']] = arg
+                    if prev_param['code'] is not None :
+                        parameter_hooks.append((prev_param['code'],prev_param['name'],dict_args[prev_param['name']]))
+
+                    needed_arguments.pop(0)
+                else :
+                    if command is None :
+                        command_name = arg
+                        if command_name in self._cl_params['commands'] :
+                            command = self._cl_params['commands'][command_name]
+                        else :
+                            self.help()
+                            self.errorexit(_("No command named [%s]") % (command_name,))
+                    else :
+                        ordered_args.append(arg)
+        if command_name is None :
+            self.help()
+            self.errorexit(_("Need a command name"))
+        if len(needed_arguments) > 0 :
+            (prev_arg_letter,prev_arg,prev_param) = needed_arguments[0]
+            self.errorexit(_("Switch [-%s] need parameter in [%s]") % (prev_arg_letter,prev_arg))
+        for code, name, value in parameter_hooks :
+            code(self, args=ordered_args, kwargs=dict_args, name=name, value=value, command=command)
+        command['code'](self,args=ordered_args,kwargs=dict_args)
             
     def run(self,args) :
         try :
